ded136d84a6aea2bd4bd951db96ae07a6cf73ac1
sakaki38 <h.sakaki38@gmail.com>
2021-07-17 23:18:48 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   get 'messages/index'
-  resources :rooms, only: [:new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end

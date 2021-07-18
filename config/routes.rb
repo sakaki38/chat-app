ce2737323c5fbb01060a56ba8fa08d9acba53abd
sakaki38 <h.sakaki38@gmail.com>
2021-07-19 00:59:57 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   get 'messages/index'
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
